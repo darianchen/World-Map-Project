be5462077d69938cd467c1a167ffb8a8b9b47fef
Darian Chen <darianychen@gmail.com>
2022-11-01 22:35:26 -0400

    Add info box and position map with box

--- src/scripts/map.js
+++ src/scripts/map.js
@@ -2,14 +2,15 @@ import * as d3 from "d3";
 import * as topojson from "topojson-client";
 
 const map = () => {
-    const height = 1100;
-    const width = 800;
-    const svg = d3.select('body').append('svg').attr('width', width).attr('height', height);
+    const height = 600;
+    const width = 1000;
+
+    const svg = d3.select('.container').append('svg').lower().attr('width', width).attr('height', height);
     const g = svg.append('g')
-    const projection = d3.geoMercator().scale(150).translate([width/2.3, height/1.5]).rotate(180,60,60);
+    const projection = d3.geoMercator().scale(150).translate([width/2.3, height/1.5]);
     const path = d3.geoPath(projection);
     
-    d3.json("src/data/world-continents.topo.json")
+    d3.json("data/world-continents.topo.json")
         .then(data => {
             const continents = topojson.feature(data, data.objects.default);
             g.selectAll(path).data(continents.features).enter().append('path').attr('class', 'continent').attr('d', path);  
