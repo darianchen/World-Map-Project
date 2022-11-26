1e664dbc4ecece26c7685a0e7e1cd904632fbfc7
Darian Chen <darianychen@gmail.com>
2022-11-25 20:43:44 -0500

    Add continent function and import styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
+import continent from "./scripts/continent";
 import worldMap from "./scripts/map"
 //import continent from "./scripts/continent.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     worldMap();
+    continent();
 });
