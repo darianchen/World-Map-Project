c1aa3585715a8b5a1bba081e65b77973b5253fcc
Darian <darianychen@gmail.com>
2023-04-25 21:39:14 -0400

    Refactor and remove code in the map file and add code to index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,45 @@
 import continent from "./scripts/continent";
 import worldMap from "./scripts/map"
-//import continent from "./scripts/continent.js"
 
 document.addEventListener("DOMContentLoaded", () => {
     worldMap();
     continent();
+
+    const aboutModal = document.getElementById('about-modal');
+    const countryModal = document.getElementById('country-modal');
+    const aboutBtn = document.getElementById('aboutBtn');
+    const closeBtns = Array.from(document.getElementsByClassName('closeBtn'));
+
+    aboutBtn.addEventListener('click', () => {
+        aboutModal.style.display = 'block';
+    })
+
+    closeBtns.forEach((closeBtn) => {
+        closeBtn.addEventListener('click', () => {
+            aboutModal.style.display = 'none';
+            countryModal.style.display = 'none';
+        })
+    })
+
+    window.addEventListener('click', (e) => {
+        document.querySelector('.currency').innerHTML = 'Currency: ';
+        if(e.target === aboutModal) aboutModal.style.display = 'none'
+
+        if(e.target === countryModal) {
+            document.querySelector('.currency').innerHTML = 'Currencies: '; 
+            countryModal.style.display = 'none'
+            clearData();
+        }
+    })
 });
+
+function clearData() {
+    const dataTags = Array.from(document.getElementsByClassName('data-tag'));
+    document.getElementById('flag').src = '#';
+
+    dataTags.forEach(el => { 
+        const child = el.firstChild;
+        el.innerHTML = '';
+        el.appendChild(child); 
+    });
+}
--- src/scripts/map.js
+++ src/scripts/map.js
@@ -10,17 +10,19 @@ async function map() {
     const projection = d3.geoMercator().scale(160).translate([505,465]);
     const path = d3.geoPath(projection);
 
-    const data = await d3.json('data/world-continents.topo.json')
+    const data = await d3.json('data/world-continents.topo.json');
     const continents = topojson.feature(data, data.objects.default);
-    g.selectAll(path).data(continents.features).enter().append('path').attr('d', path).attr('class', 'continent');
-
-    const continentPaths = document.getElementsByTagName('path');
-    continentPaths[0].setAttribute('id', 'europe');
-    continentPaths[1].setAttribute('id', 'oceania');
-    continentPaths[2].setAttribute('id', 'africa');
-    continentPaths[3].setAttribute('id', 'asia');
-    continentPaths[4].setAttribute('id', 'north-america');
-    continentPaths[5].setAttribute('id', 'south-america');
+    g.selectAll('path')
+      .data(continents.features)
+      .enter()
+      .append('path')
+      .attr('d', path)
+      .attr('class', 'continent')
+      .attr('id', (d, i) => {
+        const continentNames = ['europe', 'oceania', 'africa', 'asia', 'north-america', 'south-america'];
+        return continentNames[i];
+      });
+    
     hoverTooltip();
 
     const continentsDOM = document.getElementsByClassName('continents')[0];
@@ -39,44 +41,6 @@ async function map() {
         const continent = document.querySelector(`.${hash[name]}-container`);
         container.style.display = 'none';
         continent.style.display = 'flex';      
-    });
-
-    const aboutModal = document.getElementById('about-modal');
-    const countryModal = document.getElementById('country-modal');
-    const aboutBtn = document.getElementById('aboutBtn');
-    const closeBtns = Array.from(document.getElementsByClassName('closeBtn'));
-
-    aboutBtn.addEventListener('click', () => {
-        aboutModal.style.display = 'block';
-    });
-
-    closeBtns.forEach((closeBtn) => {
-        closeBtn.addEventListener('click', () => {
-            aboutModal.style.display = 'none';
-            countryModal.style.display = 'none';
-        })
-    }); 
-
-    window.addEventListener('click', (e) => {
-        document.querySelector('.currency').innerHTML = 'Currency: ';
-        if(e.target === aboutModal) aboutModal.style.display = 'none'
-
-        if(e.target === countryModal) {
-            document.querySelector('.currency').innerHTML = 'Currencies: '; 
-            countryModal.style.display = 'none'
-            clearData();
-        }
-    })
-}
-
-const clearData = () => {
-    const dataTags = Array.from(document.getElementsByClassName('data-tag'));
-    document.getElementById('flag').src = '#';
-
-    dataTags.forEach(el => { 
-        const child = el.firstChild;
-        el.innerHTML = '';
-        el.appendChild(child); 
     });
 }
 
